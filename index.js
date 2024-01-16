aa401acca6e154a33e1c3d49e6b41aa895e22eca
Letícia Alves <leticiadeoliveira.corp@gmail.com>
2024-01-16 13:17:42 -0300

    IntersectionObserver add

---
+++ index.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+
+    const cards = document.querySelectorAll(".services-card")
+    const observer = new IntersectionObserver(entries => {
+        console.log(entries)
+        entries.forEach(entry => {
+            const intersecting = entry.isIntersecting
+            if(screen.width < 640 || screen.height < 480){
+                entry.target.style.filter = intersecting ? "grayscale(0%)" : "grayscale(100%)"
+            }
+          })
+    },{
+        threshold:1
+    })
+
+
+    cards.forEach(card =>{
+        observer.observe(card)
+    });
+
+});
